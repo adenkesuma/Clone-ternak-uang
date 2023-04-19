3e5dfa6129ce7d7e0a72a552150f97f8a15e5bff
aden kesuma <adenfdfd10@gmail.com>
2023-04-19 14:39:00 +0700

    add javascript simple logic

---
+++ models/index.js
@@ -0,0 +1,19 @@
+popupInsight = document.querySelector(".pop-up__insight");
+popupAcademy = document.querySelector(".pop-up__academy");
+
+const whenMouseOverI = () => {
+    popupInsight.style.display = "block";
+}
+
+const whenMouseOutI = () => {
+    popupInsight.style.display = "none";
+}
+
+const whenMouseOverA = () => {
+    popupAcademy.style.display = "block";
+}
+
+const whenMouseOutA = () => {
+    popupAcademy.style.display = "none";
+}
+
